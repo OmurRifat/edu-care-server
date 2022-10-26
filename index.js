42cef3e6bd9eef4a96d5df00d4149e7d1433bcb7
OmurRifat <jabedomur.ece.ku@gmail.com>
2022-10-26 23:36:34 +0600

    data added

--- index.js
+++ index.js
@@ -3,12 +3,21 @@ const cors = require('cors');
 const app = express();
 
 app.use(cors());
-
+const facalties = require('./facalties.json')
 const port = process.env.PORT || 5000;
 
 app.get('/', (req, res) => {
-    res.send("Hey! I am working.");
+    res.send(facalties);
 });
+app.get('/facalties', (req, res) => {
+    res.send(facalties);
+});
+
+app.get('/facalties/:id', (req, res) => {
+    const id = req.params.id;
+    const facaltyData = facalties.find(facalty => facalty.id == id);
+    res.send(facaltyData);
+})
 
 app.listen(port, () => {
     console.log("Listening from PORT: ", port);
